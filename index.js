2e473a1d90c3e67e86b92f3e88ab56de2621837d
Reet Ghosh <reetghosh1@gmail.com>
2020-08-06 19:29:03 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+var server = require('http').createServer(app);
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+const listener = server.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 180000);
+
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
+
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', msg => {
+  if (msg.content === 'g!') {
+    msg.reply('Glitch OP EZ!');
+    //msg.channel.send('pong');
+
+  } else if (msg.content.startsWith('!kick')) {
+    if (msg.mentions.users.size) {
+      const taggedUser = msg.mentions.users.first();
+      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
+    } else {
+      msg.reply('Please tag a valid user!');
+    }
+  }
+  else if (msg.content === 'ping') {
+    var ping = Date.now() - msg.createdTimestamp + " ms";
+    msg.reply('My latency is: ' + ping);
+  }
+});
